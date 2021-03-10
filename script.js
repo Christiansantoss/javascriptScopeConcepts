7550f096927673b87f2615a2d120447d57d5b4f7
Christiansantoss <nismoskyi@yahoo.com>
2021-03-10 13:56:55 -0800

    hoisting best practices

--- script.js
+++ script.js
@@ -39,12 +39,51 @@
 // *************** TEMPORAL DEAD ZONE LET AND CONST ***************
 // *************** DIFFERENT TYPES OF ERROR MESSAGES ***************
 
-const myName = 'jonas';
+// const myName = 'jonas';
 
-if (myName === 'jonas') {
-  console.log(`jonas is a ${job}`); // TEMPORAL DEAD ZONE FOR JOB VARIABLE - REFERENCE ERROR CANNOT ACCESS 'JOB' BEFORE INITIALIZATION
-  const job = 'teacher';
-  const age = 2037 - 1989;
-  console.log(age);
-  console.log(x); // REFERENCE ERROR X IS NOT DEFINED
+// if (myName === 'jonas') {
+//   console.log(`jonas is a ${job}`); // TEMPORAL DEAD ZONE FOR JOB VARIABLE - REFERENCE ERROR CANNOT ACCESS 'JOB' BEFORE INITIALIZATION
+//   const job = 'teacher';
+//   const age = 2037 - 1989;
+//   console.log(age);
+//   console.log(x); // REFERENCE ERROR X IS NOT DEFINED
+// }
+
+// // Variables
+// console.log(me);
+// // console.log(job);
+// console.log(year);
+
+// var me = 'jonas';
+// // let job = 'teacher';
+// // const year = 1991;
+
+// // Functions
+// console.log(addDecl(2, 3)); // able to call the function before its declaration
+// console.log(addExpr(2, 6)); // Assigning a function value to a const or let variable is placed in the TEMPORAL DEAD ZONE!!! No access!
+// console.log(addArrow(5, 5));
+
+// //only function you can use before declaring is function declaration
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// var addArrow = (a, b) => a + b;
+
+// Example
+// clean code
+// Declare variables at the top of each scope
+// Always declare functions first and use them after declaration
+
+if (!numProducts) deleteShoppingCart(); // because of hoisting the all products gets deleted
+// even when products is 10
+
+const numProducts = 10; //dont use var to declare variables use const or let if you need to reassign a variable
+
+function deleteShoppingCart() {
+  console.log('All Products deleted');
 }
